75cdf7e0a23addf45f5d588ac294e4302f4ff6a5
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-06-12 17:21:13 -0300

    model.py
    criando a classe de Filme

--- filme/models.py
+++ filme/models.py
@@ -1,9 +1,35 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
-# criar o filme
+LISTA_CATEGORIAS = (
+    ("ANALISE", "Análise"),
+    ("PROGRAMACAO", "Programação"),
+    ("APRESENTAÇÃO", "Apresentação"),
+    ("OUTROS", "Outros"),
 
+)
+
+# criar o filme
+class Filme(models.Model):
+    titulo = models.CharField(max_length=100)
+    thumb = models.ImageField(upload_to='thumb_filmes')
+    descricao = models.TextField(max_length=1500)
+    categoria = models.CharField(max_length=100, choices=LISTA_CATEGORIAS)
+    quantidade_views = models.IntegerField(default=0)
+    data_criacao = models.DateTimeField(default=timezone.now)
+        
+    # - Filmes/Séries
+    # - thumb
+    # - titulo
+    # - descrição
+    # - categoria
+    # - quantidade de views
+    # - data da criação
+    # - Episodio
+    #     - videos
+    #     - titulo
 # criar os episódios
 
 # criar o usuário
