bfaa9c30f3931ab0f09b699b1935426ada19f25b
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-06-13 13:47:33 -0300

    Update urls.py

--- interflix/urls.py
+++ interflix/urls.py
@@ -23,8 +23,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('/', include('filme.urls')),
+    path('', include('filme.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
