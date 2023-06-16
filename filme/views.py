01b1cd6885f37c6170a99c04e9c66403562467e1
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-06-16 17:44:44 -0300

    Update views.py

--- filme/views.py
+++ filme/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 # Create your views here.
 def homepage(request):
     return render(request, 'homepage.html')
+
+# url - view - template
+def homefilmes(request):
+    return render(request, 'homefilmes.html')
