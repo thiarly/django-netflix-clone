ff8f2cac8affd9dd072999cdcbd49c4fb2c72811
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-06-20 23:00:09 -0300

    Update views.py

--- filme/views.py
+++ filme/views.py
@@ -2,15 +2,23 @@
 
 from django.shortcuts import render
 from .models import Filme
+from django.views.generic import TemplateView, ListView
 
-# Create your views here.
-def homepage(request):
-    return render(request, 'homepage.html')
+# # Create your views here.
+# def homepage(request):
+#     return render(request, 'homepage.html')
 
-# url - view - template
-def homefilmes(request):
-    context = {}
-    lista_filmes = Filme.objects.all()
-    context['lista_filmes'] = lista_filmes
-    return render(request, 'homefilmes.html', context)
+class Homepage(TemplateView):
+    template_name = 'homepage.html'
+
+# # url - view - template
+# def homefilmes(request):
+#     context = {}
+#     lista_filmes = Filme.objects.all()
+#     context['lista_filmes'] = lista_filmes
+#     return render(request, 'homefilmes.html', context)
  
+class Homefilmes(ListView):
+    template_name = 'homefilmes.html'
+    model = Filme
+     
