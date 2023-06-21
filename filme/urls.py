4cde2ffdc844315d5871da790f444259de87c5c0
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-06-20 23:00:06 -0300

    Update urls.py

--- filme/urls.py
+++ filme/urls.py
@@ -1,8 +1,8 @@
 # url - view - template
 from django.urls import path, include
-from .views import homepage, homefilmes
+from .views import Homefilmes, Homepage
 
 urlpatterns = [
-    path('', homepage),
-    path('filmes/', homefilmes),
+    path('', Homepage.as_view()),
+    path('filmes/', Homefilmes.as_view()),
 ]
