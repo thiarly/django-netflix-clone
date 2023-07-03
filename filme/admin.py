ca563f7f0aa6bc55f6290fe074f7d895109a100b
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-07-03 11:47:48 -0300

    Update admin.py

--- filme/admin.py
+++ filme/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Filme
+from .models import Filme, Episodio
 
 # Register your models here.
 admin.site.register(Filme)
+admin.site.register(Episodio)
 
 
 
