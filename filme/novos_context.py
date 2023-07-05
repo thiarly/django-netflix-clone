b85909fa945d73da4915263a01c891bb6c9246cd
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-07-05 11:05:44 -0300

    create new context

---
+++ filme/novos_context.py
@@ -0,0 +1,11 @@
+from .models import Filme
+
+
+def lista_filmes_recentes(request):
+    lista_filmes = Filme.objects.all().order_by('-data_criacao')[0:10]
+    return {'lista_filmes_recentes': lista_filmes}
+
+
+def lista_filmes_emalta(request):
+    lista_filmes = Filme.objects.all().order_by('-quantidade_views')[0:10]
+    return {'lista_filmes_emalta': lista_filmes}
