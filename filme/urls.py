563d91a5e62df34bee4cd66654f2e4ee79a027a9
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-07-06 14:26:15 -0300

    Create search moveis

--- filme/urls.py
+++ filme/urls.py
@@ -1,6 +1,6 @@
 # url - view - template
 from django.urls import path, include
-from .views import Homefilmes, Homepage, Detalhesfilme
+from .views import Homefilmes, Homepage, Detalhesfilme, Pesquisafilme
 
 app_name = 'filme'
 
@@ -9,5 +9,7 @@ urlpatterns = [
     path('', Homepage.as_view(), name='homepage'),
     path('filmes/', Homefilmes.as_view(), name='homefilmes'),
     path('filmes/<int:pk>', Detalhesfilme.as_view(), name='detalhesfilme'),
+    path('pesquisa/', Pesquisafilme.as_view(), name='pesquisafilme'),
 ]
 
+ 
--- filme/views.py
+++ filme/views.py
@@ -37,7 +37,19 @@ class Detalhesfilme(DetailView):
      
     def get_context_data(self, **kwargs):
         context = super(Detalhesfilme, self).get_context_data(**kwargs)
-        filmes_relacionados = Filme.objects.filter(categoria=self.get_object().categoria)[0:5]
+        filmes_relacionados = self.model.objects.filter(categoria=self.get_object().categoria)[0:5]
         context['filmes_relacionados'] = filmes_relacionados
         return context
-    
+
+
+class Pesquisafilme(ListView):
+    template_name = 'pesquisa.html'
+    model = Filme
+
+    def get_queryset(self):
+        termo_pesquisa = self.request.GET.get('query')
+        if termo_pesquisa:
+            object_list = self.model.objects.filter(titulo__icontains=termo_pesquisa)
+            return object_list
+        else:
+            return None
