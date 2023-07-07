40d16585b2949b86564a373df3a985cbdd7b86f6
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-07-07 15:40:48 -0300

    Update urls.py

--- filme/urls.py
+++ filme/urls.py
@@ -1,5 +1,5 @@
 # url - view - template
-from django.urls import path, include
+from django.urls import path, reverse_lazy
 from .views import Homefilmes, Homepage, Detalhesfilme, Pesquisafilme, Paginaperfil, Criarconta
 from django.contrib.auth import views as auth_views
 
@@ -14,8 +14,10 @@ urlpatterns = [
     path('pesquisa/', Pesquisafilme.as_view(), name='pesquisafilme'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
-    path('editarperfil/', Paginaperfil.as_view(), name='editarperfil'),
+    path('editarperfil/<int:pk>', Paginaperfil.as_view(), name='editarperfil'),
     path('criarcontar/', Criarconta.as_view(), name='criarconta'),
+    path('mudarsenha/', auth_views.PasswordChangeView.as_view(template_name='editarperfil.html',
+                                    success_url=reverse_lazy('filme:homefilmes')), name='mudarsenha'),
 ]
 
  
