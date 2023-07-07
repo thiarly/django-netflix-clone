33da0b320e0592e683650e3afa4095cdfacd21e6
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-07-07 11:22:42 -0300

    Update settings.py

--- interflix/settings.py
+++ interflix/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'filme',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -135,6 +137,12 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = 'filme:homefilme'
+LOGIN_REDIRECT_URL = 'filme:homefilmes'
 
 LOGIN_URL = 'filme:login'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+#https://github.com/django-crispy-forms/crispy-bootstrap5
