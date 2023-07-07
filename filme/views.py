42b025df6515d71eaa092ac624960d3747fa01e0
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-07-07 11:22:40 -0300

    Update views.py

--- filme/views.py
+++ filme/views.py
@@ -1,8 +1,8 @@
 # url - view - template
-
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from .models import Filme
-from django.views.generic import TemplateView, ListView, DetailView
+from .forms import CriarContaForm
+from django.views.generic import TemplateView, ListView, DetailView, FormView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
@@ -59,3 +59,18 @@ class Pesquisafilme(LoginRequiredMixin, ListView):
             return object_list
         else:
             return None
+        
+class Paginaperfil(LoginRequiredMixin, TemplateView):
+    template_name = 'editarperfil.html'
+    
+    
+class Criarconta(FormView):
+    template_name = 'criarconta.html'
+    form_class = CriarContaForm
+    
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse('filme:login')
