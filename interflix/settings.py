b432583128615de2fe3d7377f9cb370bb654edd8
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-07-08 13:07:08 -0300

    Update settings.py

--- interflix/settings.py
+++ interflix/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 TOKEN_CSRF = os.getenv('TOKEN_CSRF')
 
 if TOKEN_CSRF:
-    TOKEN_CSRF = TOKEN_CSRF
-    CRSF_TRUSTED_ORIGINS = ['https://django-netflix-clone-production.up.railway.app']
+    SECRET_KEY = TOKEN_CSRF
+    CSRF_TRUSTED_ORIGINS = ['https://django-netflix-clone-production.up.railway.app']
 else:
     SECRET_KEY = 'django-insecure-=y8!rkd_@5_!5#ta!fwno1k48yfvl(84ow9qa#kt@&r^8ov#19'
 
