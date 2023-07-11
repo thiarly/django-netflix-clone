ff0dc4defc6d482c1feb166590a50b9440173ee4
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-07-11 17:05:40 -0300

    update

--- filme/apps.py
+++ filme/apps.py
@@ -6,15 +6,3 @@ class FilmeConfig(AppConfig):
     name = 'filme'
     
     
-    def ready(self):
-        from .models import Usuario
-        import os
-        
-        email = os.getenv("EMAIL-ADMIN")
-        senha = os.getenv("SENHA-ADMIN")
-        
-        usuarios = Usuario.objects.filter(email=email)
-        if not usuarios:
-            Usuario.objects.create_superuser(username="admin", email=email, password=senha, is_active=True, is_staff=True)
-                
-    
